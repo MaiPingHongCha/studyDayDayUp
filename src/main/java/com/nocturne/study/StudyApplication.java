078384f4575703c1f78d62a010a17dedd68485dd
dingliang <dl_nocturne@163.com>
2019-04-23 00:04:27 +0800

    添加注释

--- src/main/java/com/nocturne/study/StudyApplication.java
+++ src/main/java/com/nocturne/study/StudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyApplication {
 
+    //给个注释
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
     }
